4709ad4e06a6ac225c7bdcab2bc75ab5dd95c880
YuanLiuuuuuu <3463423099@qq.com>
2024-12-02 22:58:04 +0800

    [Feature]: Add image processor

--- wepoints/utils/images/__init__.py
+++ wepoints/utils/images/__init__.py
@@ -1,5 +1,4 @@
 from .catty import split_image_with_catty
+from .qwen2vl_image_processor import Qwen2ImageProcessorForPOINTSV15
 
-__all__ = [
-    'split_image_with_catty',
-]
+__all__ = ['split_image_with_catty', 'Qwen2ImageProcessorForPOINTSV15']

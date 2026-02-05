64a5d82d79fe4167e58a0268a36260614371cb90
Yuan Liu <30762564+YuanLiuuuuuu@users.noreply.github.com>
2026-02-05 11:55:13 +0800

    [Feature]: Add qwen3vl navit (#44)
    * [Feature]: Add qwen3vl navit
    
    * [Feature]: Delete rope params

--- wepoints/models/vision/__init__.py
+++ wepoints/models/vision/__init__.py
@@ -1,7 +1,9 @@
 from .qwen2_5_vl_navit import Qwen2_5VisionTransformerForNavitPOINTS
 from .qwen2_vl_navit import Qwen2VisionTransformerForNavitPOINTS
+from .qwen3_vl_navit import Qwen3VLVisionModelForNavitPOINTS
 
 __all__ = [
     'Qwen2VisionTransformerForNavitPOINTS',
-    'Qwen2_5VisionTransformerForNavitPOINTS'
+    'Qwen2_5VisionTransformerForNavitPOINTS',
+    'Qwen3VLVisionModelForNavitPOINTS'
 ]
--- wepoints/models/vision/qwen3_vl_navit.py
+++ wepoints/models/vision/qwen3_vl_navit.py
@@ -1,13 +1,11 @@
 import torch
 import torch.nn.functional as F
-from transformers.models.qwen2_vl.modeling_qwen2_vl import \
-    Qwen2VisionTransformerPretrainedModel  # noqa
+from transformers.models.qwen3_vl.modeling_qwen3_vl import Qwen3VLVisionModel
 
 
-class Qwen2VisionTransformerForNavitPOINTS(
-        Qwen2VisionTransformerPretrainedModel):  # noqa
-    """Rewrite the forward function of Qwen2VisionTransformerPretrainedModel to
-    adapt to POINTS.  # noqa.
+class Qwen3VLVisionModelForNavitPOINTS(Qwen3VLVisionModel):
+    """Rewrite the forward function of Qwen3VLVisionModel to
+    adapt to POINTS.
 
     Do no apply patch merging to the hidden features output by the transformer.
     """
@@ -17,7 +15,15 @@ class Qwen2VisionTransformerForNavitPOINTS(
     def forward(self, hidden_states: torch.Tensor,
                 grid_thw: torch.Tensor) -> torch.Tensor:
         hidden_states = self.patch_embed(hidden_states)
+
+        pos_embeds = self.fast_pos_embed_interpolate(grid_thw)
+        hidden_states = hidden_states + pos_embeds
+
         rotary_pos_emb = self.rot_pos_emb(grid_thw)
+
+        seq_len, _ = hidden_states.size()
+        hidden_states = hidden_states.reshape(seq_len, -1)
+        rotary_pos_emb = rotary_pos_emb.reshape(seq_len, -1)
         emb = torch.cat((rotary_pos_emb, rotary_pos_emb), dim=-1)
         position_embeddings = (emb.cos(), emb.sin())
 
@@ -26,9 +32,8 @@ class Qwen2VisionTransformerForNavitPOINTS(
                                                  dim=0, dtype=torch.int32)
         cu_seqlens = F.pad(cu_seqlens, (1, 0), value=0)
 
-        for blk in self.blocks:
+        for layer_id, blk in enumerate(self.blocks):
             hidden_states = blk(hidden_states,
                                 cu_seqlens=cu_seqlens,
                                 position_embeddings=position_embeddings)
-
         return hidden_states
